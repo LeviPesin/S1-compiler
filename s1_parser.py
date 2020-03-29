52a7bf859cc05ef4b6e00eabfa9680c64436c407
LeviPesin <35454228+LeviPesin@users.noreply.github.com>
2020-03-30 00:03:35 +1030

    Add files via upload

---
+++ s1_parser.py
@@ -0,0 +1,319 @@
+class AST:
+	pass
+
+class IfE(AST):
+	def __init__(self, left, right):
+		self.cond = left
+		self.stat = right
+
+class IfNe(AST):
+	def __init__(self, left, right):
+		self.cond = left
+		self.stat = right
+
+class WhileE(AST):
+	def __init__(self, left, right):
+		self.cond = left
+		self.stat = right
+
+class WhileNe(AST):
+	def __init__(self, left, right):
+		self.cond = left
+		self.stat = right
+
+class Func(AST):
+	def __init__(self, left, right):
+		self.name = left
+		self.pars = []
+		self.code = right
+		
+class StatsList(AST):
+	def __init__(self):
+		self.stats = []
+
+class Program(AST):
+	def __init__(self):
+		self.blocks = []
+
+class Assign(AST):
+	def __init__(self, left, right):
+		self.var = left
+		self.value = right
+
+class FuncCall(AST):
+	def __init__(self, left):
+		self.name = left
+		self.pars = []
+
+class BinOp(AST):
+	def __init__(self, left, op, right):
+		self.left = left
+		self.op = op
+		self.right = right
+
+class NoOp(AST):
+	def __init__(self):
+		pass
+
+class Var(AST):
+	def __init__(self, token):
+		self.name = token.value
+
+class Set(AST):
+	def __init__(self):
+		self.pars = []
+
+class Parser:
+	def __init__(self, lexer):
+		self.lexer = lexer
+		self.current_token = self.lexer.get_next_token()
+		
+	def next(self):
+		self.current_token = self.lexer.get_next_token()
+	
+	def parse(self):
+		node = self.program()
+		assert self.current_token.type == 'EOF'
+		return node
+	
+	def program(self):
+		node = Program()
+		while (self.current_token != 'EOF'):
+			node.blocks.append(self.block())
+		return node
+	
+	def block(self):
+		if self.current_token.type == 'IFE':
+			return self.ife()
+		elif self.current_token.type == 'IFNE':
+			return self.ifne()
+		elif self.current_token.type == 'WHIE':
+			return self.whilee()
+		elif self.current_token.type == 'WHINE':
+			return self.whilene()
+		else:
+			assert self.current_token.type == 'ID':
+			pos = self.lexer.pos
+			try:
+				return self.func()
+			except:
+				self.lexer.pos = pos
+				return self.statement_list()
+	
+	def ife(self):
+		assert self.current_token.type == 'IFE'
+		next()
+		assert self.current_token.type == 'LBR'
+		next()
+		var = self.var()
+		assert self.current_token.type == 'RBR'
+		next()
+		assert self.current_token.type == 'LCBR'
+		stats_list = self.statement_list()
+		assert self.current_token.type == 'RCBR'
+		next()
+		return IfE(var, stats_list)
+	
+	def ifne(self):
+		assert self.current_token.type == 'IFNE'
+		next()
+		assert self.current_token.type == 'LBR'
+		next()
+		var = self.var()
+		assert self.current_token.type == 'RBR'
+		next()
+		assert self.current_token.type == 'LCBR'
+		stats_list = self.statement_list()
+		assert self.current_token.type == 'RCBR'
+		next()
+		return IfNe(var, stats_list)
+	
+	def whilee(self):
+		assert self.current_token.type == 'WHIE'
+		next()
+		assert self.current_token.type == 'LBR'
+		next()
+		var = self.var()
+		assert self.current_token.type == 'RBR'
+		next()
+		assert self.current_token.type == 'LCBR'
+		stats_list = self.statement_list()
+		assert self.current_token.type == 'RCBR'
+		next()
+		return WhileE(var, stats_list)
+	
+	def whilene(self):
+		assert self.current_token.type == 'WHINE'
+		next()
+		assert self.current_token.type == 'LBR'
+		next()
+		var = self.var()
+		assert self.current_token.type == 'RBR'
+		next()
+		assert self.current_token.type == 'LCBR'
+		stats_list = self.statement_list()
+		assert self.current_token.type == 'RCBR'
+		next()
+		return WhileNe(var, stats_list)
+		
+	def func(self):
+		name = self.func_name()
+		assert self.current_token.type == 'LBR'
+		next()
+		pars = self.formparams()
+		assert self.current_token.type == 'RBR'
+		next()
+		assert self.current_token.type == 'LCBR'
+		stats_list = self.func_statement_list()
+		assert self.current_token.type == 'RCBR'
+		next()
+		node = Func(name, stats_list)
+		node.pars = pars
+		next()
+		return node
+		
+	def formparams(self):
+		node = []
+		while self.current_token.type == 'ID':
+			node.append(self.var())
+			if self.current_token.type != 'COMMA':
+				next()
+				assert self.current_token.type != 'ID'
+		if node == []:
+			next()
+		return node
+		
+	def var(self):
+		assert self.current_token.type == 'ID'
+		token = self.current_token
+		next()
+		return Var(token)
+	
+	def func_name(self):
+		assert self.current_token.type == 'ID'
+		token = self.current_token
+		next()
+		return token.value
+		
+	def statement_list(self):
+		stats_list = []
+		while True:
+			pos = self.lexer.pos
+			try:
+				stats_list.append(self.statement())
+			except:
+				self.lexer.pos = pos
+				break
+		return stats_list
+	
+	def func_statement_list(self):
+		stats_list = []
+		while True:
+			pos = self.lexer.pos
+			try:
+				stats_list.append(self.statement())
+			except:
+				self.lexer.pos = pos
+				try:
+					stats_list.append(self.func_statement())
+				except:
+					self.lexer.pos = pos
+					break
+		return stats_list
+	
+	def statement(self):
+		if self.current_token.type == 'EMP':
+			next()
+			return NoOp()
+		elif self.current_token.type == 'LCOMM':
+			next()
+			return NoOp()
+		else:
+			assert self.current_token.type == 'ID'
+			pos = self.lexer.pos
+			try:
+				node = self.assign()
+				assert self.current_token.type == 'SEMICOL'
+				next()
+				return node
+			except:
+				node = self.funcall()
+				assert self.current_token.type == 'SEMICOL'
+				next()
+				return node
+				
+	def params(self):
+		node = []
+		signal = False
+		signal2 = True
+		while True:
+			try:
+				if signal:
+					signal2 = False
+				node.append(self.expr())
+				next()
+				if self.current_token.type != 'COMMA':
+					signal = True
+			except:
+				assert signal2
+		if node == []:
+			next()
+		return node
+		
+	def assign(self):
+		assert self.current_token.type == 'ID'
+		var = self.var()
+		assert self.current_token.type == 'ASSIGN'
+		next()
+		expr = self.expr()
+		return Assign(var, expr)
+		
+	def funcall(self):
+		assert self.current_token.type == 'ID'
+		node = FuncCall(self.func_name())
+		assert self.current_token.type == 'LBR'
+		next()
+		pars = self.params()
+		node.pars = pars
+		assert self.current_token.type == 'RBR'
+		next()
+		return node
+	
+	def expr(self):
+		try:
+			node = self.term()
+			return node
+		else:
+			left = self.expr()
+			token = self.current_token
+			assert token.type in ['UNION', 'INTER', 'DIFF', 'SYMDIFF']
+			right = self.expr()
+			return BinOp(left, token, right)
+	
+	def term(self):
+		if self.current_token.type == 'LSBR':
+			return self.set1()
+		elif self.current_token.type == 'LBR':
+			next()
+			node = self.expr()
+			assert self.current_token.type == 'RBR'
+			next()
+			return node
+		else:
+			assert self.current_token.type == 'ID'
+			pos = self.lexer.pos
+			try:
+				node = self.funcall()
+				return node
+			except:
+				self.lexer.pos = pos
+				node = self.var()
+				return node
+	
+	def set1(self):
+		assert self.current_token.type == 'LSBR'
+		pars = self.params()
+		node = Set()
+		node.pars = pars
+		assert self.current_token.type == 'RSBR'
+		return node
